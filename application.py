65c1c25886887af9658957275d9140b5650a8c7b
한준규 <41336726+doongu@users.noreply.github.com>
2021-05-13 18:37:35 +0900

    Update application.py
    ATM_Controller ver2

--- application.py
+++ application.py
@@ -1,17 +1,5 @@
-from ATM_CONTROLLER import conn_db
 import pymysql
-
-#나는 몽고인지 MYSQL인지 다른건지 모른다.
-#그래서 둘 다 쿼리문으로 확인을 해보고 없으면, MYSQL이나 MONGODB인지 확인해달라고 해야함.
-#mysql 쿼리문
-#select * from emp where empno >0
-# 테이블(emp) , * (모든 컬럼), empno > 0 (조건)
-#mongodb 쿼리문
-#db.emp.find({empno:{$gt:3}})
-# 테이블(emp), find(모든컬럼), empno:{$gt:3} (조건)
-
-#developer가 써줘야하는 부분
-#developer가
+from ATM_CONTROLLER import DB
 
 curs = ""
 def db (host, user, password, db, port, charset= 'utf8'):
@@ -19,20 +7,16 @@ def db (host, user, password, db, port, charset= 'utf8'):
     conn = pymysql.connect(host=host, user=user, password=password, db=db, port=port, charset=charset)
     curs = conn.cursor()
 
-def select_pin(collom_name, table_name):
+def select_pin(collom_name, table_name, condition):
     global curs
     query = "select %s from %s" % (collom_name, table_name) #사용자 작성 쿼리문
     curs.execute(query)
     print(curs.fetchall())
 
+Developer_db = DB() #연결하는 생성자 생성
+Developer_db.db_connect(db, "localhost", "root", "asd", "study", 3306, "utf8")
+Developer_db.select_connect(select_pin,"*" , "study_test")
 
-#input
-Developer_db = conn_db(db) #연결하는 생성자 생성
-#데이터 값 가져오는 생성자 생성
-#conn_db
-Developer_db.conn("localhost", "root", "asd", "study", 3306, "utf8")
-Developer_db.select_pin_number(select_pin)
-Developer_db.select("id", "test")
-# Sellector.select_pin_number("id", "test")
-
+Developer_db.connect()
+Developer_db.select()
 
