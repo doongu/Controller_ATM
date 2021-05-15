0333c479912d8985cda749c33626534cc974e9e5
한준규 <41336726+doongu@users.noreply.github.com>
2021-05-15 21:57:44 +0900

    Update ATM_CONTROLLER.py
    ATM ver3

--- ATM_CONTROLLER.py
+++ ATM_CONTROLLER.py
@@ -1,38 +1,74 @@
 class DB:
     # 클래스의 생성자 항상 함수 형태를 가지고 init이라는 이름 을 가지고 self를 기본으로 가짐
 
+    #db연결 함수 생성
     def db_connect(self, db_collect, *args):
         self.db_connect_args = []
         for i in args:
             self.db_connect_args.append(i)
         self.db_collect = db_collect
 
+    #select 함수 생성
     def select_connect(self, select_def, *args):
         self.db_select_args = []
         for i in args:
             self.db_select_args.append(i)
         self.select_def = select_def
 
+    #db연결 함수 실행
     def connect(self):
         self.db_collect(*self.db_connect_args)
+
+    #db연결 함수 생성
     def select(self):
         self.select_def(*self.db_select_args)
 
-class Controll(DB):
-    def __init__(self, Controller):
-        self.Controller = Controller
-    #확인
-    def confirm_money(self, confirm):
-        self.confirm = confirm
+class Controll:
+    #확인 함수 생성
+    def confirm_money(self, confirm_func, *confirm_args):
+        self.confirm_list = []
+        for i in confirm_args:
+            self.confirm_list.append(i)
+        self.confirm_func = confirm_func
+    #입금 함수 생성
+    def deposit_money(self, deposit_func, *deposit_args):
+        self.deposit_list = []
+        for i in deposit_args:
+            self.deposit_list.append(i)
+        self.deposit_func = deposit_func
+    #출금 함수 생성
+    def withdraw_money(self, withdraw_func, *withdraw_args):
+        self.withdraw_list = []
+        for i in withdraw_args:
+            self.withdraw_list.append(i)
+        self.withdraw_func = withdraw_func
 
-    #입금
-    def deposit_money(self, deposit):
-        self.deposit = deposit
+    #money 확인 함수 실행
+    def confirm_exe(self):
+        self.confirm_func(*self.confirm_list)
 
-    #출금
-    def withdraw_money(self, withdraw):
-        self.withdraw = withdraw
+    #입금 함수 실행
+    def deposit_exe(self):
+        self.deposit_func(*self.deposit_list)
 
+    #출금 함수 실행
+    def withdraw_exe(self):
+        self.withdraw_func(*self.withdraw_list)
+
+#list로 입력을 받는다.
+def My_Pin_Format(*pin):
+    retun_pin_format = []
+    for i in pin:
+        if PIN(i):
+            return_pin_format.append(PIN(i))
+        else:
+            print("문자열을 제대로 입력해주세요.")
+            return False
+    else:
+        print("당신 은행의 포맷형태들은 다음과 같습니까?")
+        for i in retun_pin_format:
+            print(i)
+        return retun_pin_format
 
 def PIN(pin):
     #벨리데이션
